07708ec4254254895905616f697a17f8aab8b351
saowang <saowang@outlook.com>
2020-08-09 17:02:21 +0800

    Add documents and cloudflare ns action

---
+++ ns.js
@@ -0,0 +1,12 @@
+module.exports = {
+    cname: {
+        "80c51a872a783ec5f2414773ac6bc26c": "verify.bing.com",
+        "rundocs.io": "rundocs.github.io",
+        /* start */
+        "jekyll-rtd-theme": "rundocs.github.io",
+        "saowang": "saowang.github.io",
+    },
+    txt: {
+        "rundocs.io": "google-site-verification=bnnUUSDymPpDsVxL4OVeq9IAWSHnsm_1G2Zw1K9E8R4",
+    }
+}
