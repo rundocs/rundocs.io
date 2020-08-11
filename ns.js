414406961e1de696d2b622c22d219d3ee98bed9e
saowang <68011645+saowang@users.noreply.github.com>
2020-08-11 11:50:23 +0800

    Update ns.js

--- ns.js
+++ ns.js
@@ -2,9 +2,6 @@ module.exports = {
     cname: {
         "rundocs.io": "rundocs.github.io",
         /* start */
-        "analytics": "rundocs.github.io",
-        "jekyll-rtd-theme": "rundocs.github.io",
-        "saowang": "saowang.github.io",
     },
     txt: {
         "rundocs.io": "google-site-verification=bnnUUSDymPpDsVxL4OVeq9IAWSHnsm_1G2Zw1K9E8R4",
