47d6612c9515b082efca2468ea8c01677d952c68
saowang <68011645+saowang@users.noreply.github.com>
2020-08-17 19:24:06 +0800

    Add jekyll-themes.rundocs.io

--- ns.js
+++ ns.js
@@ -2,6 +2,7 @@ module.exports = {
     cname: {
         "rundocs.io": "rundocs.github.io",
         /* start */
+        "jekyll-themes": "rundocs.github.io",
     },
     txt: {
         "rundocs.io": "google-site-verification=bnnUUSDymPpDsVxL4OVeq9IAWSHnsm_1G2Zw1K9E8R4",
